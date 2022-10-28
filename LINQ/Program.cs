135e44848dcb366779a1cb9cf59eeeaa8f176eff
Jonathan-Wills <wills830@hotmail.com>
2022-10-28 17:02:38 -0400

    initial commit

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -1,10 +1,23 @@
-﻿namespace LINQ
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace LINQ
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var games = new List<string>
+            {"Call of Duty", "Terraria","Minecraft", "Tetris"};
+            
+            foreach (var game in games)
+            {
+                Console.WriteLine(game);
+            }
+
+            var two = games.OrderByDescending(x => x.Length).ToList();
         }
     }
 }
